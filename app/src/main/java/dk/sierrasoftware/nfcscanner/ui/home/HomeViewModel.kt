9bd9dfe0f54188ffe7be9d8b36ac7a675ae5f8ea
Mark Jensen <maje@trifork.com>
2024-08-29 22:21:53 +0200

    snapshot

--- app/src/main/java/dk/sierrasoftware/nfcscanner/ui/home/HomeViewModel.kt
+++ app/src/main/java/dk/sierrasoftware/nfcscanner/ui/home/HomeViewModel.kt
@@ -6,13 +6,5 @@ import androidx.lifecycle.ViewModel
 import dk.sierrasoftware.nfcscanner.api.EnrichedEntityDTO
 
 class HomeViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-
     val entityClosure = MutableLiveData<EnrichedEntityDTO?>()
-
-
-    val text: LiveData<String> = _text
 }
